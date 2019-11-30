53203f8e37a341d31a9ada8a98347ac7457abc7c
bacon <baconpap@gmail.com>
2019-11-30 12:13:58 +0800

    change ContextTests structure test cases for different load mod of singleton

--- PapaLib.Tests/IOC/ContextTests.cs
+++ PapaLib.Tests/IOC/ContextTests.cs
@@ -6,13 +6,16 @@ namespace PapaLib.Tests.IOC
 {
     public class ContextTests
     {
-        private Context context;
+        protected Context context;
 
         public ContextTests()
         {
             context = new Context();
         }
+    }
 
+    public class ContextInstanceReferenceTests : ContextTests
+    {
         private interface IGUIDGenerator
         {
             Guid guid {get;}
@@ -61,6 +64,36 @@ namespace PapaLib.Tests.IOC
             var instance1 = context.GetInstance<IGUIDGenerator>();
             var instance2 = context.GetInstance<IGUIDGenerator>();
             Assert.False(instance1.guid == instance2.guid);
+        }
+    }
+
+    public class ContextSingletonLoadModTests : ContextTests
+    {
+        public ContextSingletonLoadModTests() : base()
+        {
+            LoadingChecker.instantiateCount = 0;
+        }
+        private class LoadingChecker
+        {
+            public static int instantiateCount = 0;
+            public LoadingChecker()
+            {
+                instantiateCount ++;
+            }
+        }
+        [Fact]
+        public void RegisterSingletonForClass_InTimeMod()
+        {
+            context.RegisterSingleton<LoadingChecker>();
+            Assert.Equal(1, LoadingChecker.instantiateCount);
+        }
+        [Fact]
+        public void RegisterSingletonForClass_LazyMod()
+        {
+            context.RegisterSingleton<LoadingChecker>(Context.SingletonLoadMode.Lazy);
+            Assert.Equal(0, LoadingChecker.instantiateCount);
+            context.GetInstance<LoadingChecker>();
+            Assert.Equal(1, LoadingChecker.instantiateCount);
         }
     }
 }
